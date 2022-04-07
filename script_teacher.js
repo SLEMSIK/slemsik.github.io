0d411cb16409c007b0ed9a7f1e2b712e884f08a6
SLEMSIK <84916321+SLEMSIK@users.noreply.github.com>
2022-04-07 09:40:42 +0300

    Update script_teacher.js

--- script_teacher.js
+++ script_teacher.js
@@ -1,16 +1,5 @@
-// Requiring the module
-const reader = require( 'xlsx' )
-// Reading our test file
-const file = reader.readFile( './test.xlsx' )
-let data = []
-const sheets = file.SheetNames
-for (let i = 0; i < sheets.length; i++)
-{
-const temp = reader.utils.sheet_to_json(
-file.Sheets[file.SheetNames[i]])
-temp.forEach((res) => {
-data.push(res)
-})
-}
-// Printing data
-console.log(data)
+var count = 0;
+count = count + 5;
+console.log(count);
+count = count + 3;
+console.log(count);
