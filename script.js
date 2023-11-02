e75f2664132530c0e66b5d82c33467b7bd3312ec
Vyacheslav Pechenov <Vya-job@mail.ru>
2023-11-02 12:15:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-alert(navigator.platform)
+mobile = navigator.maxTouchPoints > 0 && 'orientation' in window
+if(mobile==true){
+	 window.location.href = `mobile.html`
+}
 function click_hundler(id) {
 	 window.location.href = `html/${id}.html`
 }
